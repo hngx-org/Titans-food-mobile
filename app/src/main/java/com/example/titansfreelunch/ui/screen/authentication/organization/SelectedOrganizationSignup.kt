f16cfe6a8947a10edaeef73f06fa73384a331482
githukueliud <githukueliud@gmail.com>
2023-09-21 14:19:20 +0300

    Staff and organization auth tree

---
+++ app/src/main/java/com/example/titansfreelunch/ui/screen/authentication/organization/SelectedOrganizationSignup.kt
@@ -0,0 +1,162 @@
+package com.example.titansfreelunch.ui.screen.authentication.organization
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.SmallFloatingActionButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.titansfreelunch.R
+
+@Composable
+fun SelectedOrganizationSignup(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(R.string.welcome_to_lu),
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(20.dp)
+        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = Color(214, 218, 225))
+        ) {
+            Column(
+                verticalArrangement = Arrangement.SpaceBetween,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxSize()
+            ){
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ){
+                    Text(
+                        text = stringResource(R.string.sign_up_as),
+                        modifier = Modifier.padding(10.dp),
+                        fontSize = 20.sp
+                    )
+                    Row {
+                        Card(
+                            modifier = Modifier
+                                .height(100.dp)
+                                .width(180.dp),
+                            colors = CardDefaults.cardColors(
+                                containerColor = Color.White
+                            )
+                        ) {
+                            Column(
+                                modifier = Modifier.fillMaxSize(),
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                verticalArrangement = Arrangement.Center
+                            ) {
+                                Text(
+                                    text = stringResource(R.string.staff),
+                                    fontSize = 18.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    modifier = Modifier.padding(bottom = 10.dp)
+                                )
+                                Icon(
+                                    painter = painterResource(id = R.drawable.people),
+                                    contentDescription = stringResource(R.string.staff_signup),
+                                    modifier = Modifier.size(24.dp)
+                                )
+                            }
+
+                        }
+                        Spacer(modifier = Modifier.width(10.dp))
+                        Card(
+                            modifier = Modifier
+                                .height(100.dp)
+                                .width(180.dp),
+                            colors = CardDefaults.cardColors(
+                                containerColor = Color.White
+                            ),
+                            border = BorderStroke(1.dp, Color.Black)
+                        ) {
+                            Column(
+                                modifier = Modifier.fillMaxSize(),
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                verticalArrangement = Arrangement.Center
+                            ) {
+                                Text(
+                                    text = stringResource(R.string.organization),
+                                    fontSize = 18.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    modifier = Modifier.padding(bottom = 10.dp)
+                                )
+                                Icon(
+                                    painter = painterResource(id = R.drawable.organization),
+                                    contentDescription = stringResource(R.string.organization_signup),
+                                    modifier = Modifier.size(24.dp)
+                                )
+                            }
+
+                        }
+                    }
+                }
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.End
+                ){
+                    SmallFloatingActionButton(
+                        onClick = { /*TODO*/ },
+                        containerColor = Color(6, 59, 39),
+                        modifier = Modifier
+                            .padding(end = 10.dp)
+                            .size(50.dp)
+                            .clip(RoundedCornerShape(50))
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.arrow_right),
+                            contentDescription = stringResource(id = R.string.next_step),
+                            modifier = Modifier.size(30.dp)
+                        )
+                    }
+                    Spacer(
+                        modifier = Modifier.height(20.dp)
+                    )
+                }
+
+            }
+
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SignupScreePreview() {
+    SelectedOrganizationSignup()
+}
