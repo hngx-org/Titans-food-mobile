d790d4882d1277ea1e4046f1f5b51eb5be7c772a
Eliud Githuku <138256399+githukueliud@users.noreply.github.com>
2023-09-22 23:13:39 +0300

    Auth tree (#34)
    * testing branch push
    
    * created create profile screen
    
    * enter email screen
    
    * verify email screen
    
    * Add people screen
    
    * Staff and organization auth tree
    
    * auth screens ui done
    
    * auth screens ui done
    
    * auth screens ui done
    
    * git ignored files
    
    * auth screens ui done
    
    * auth screens ui done
    
    * Feature redeem lunch (#16)
    
    * Feature Redeem Launch
    
    * Feature Redeem Launch
    
    * Feature Hi_Login Screen (#27)
    
    Co-authored-by: Daniel <danieliroka67@gmail.com>
    
    * small fix with duplicate naming
    
    * Base structure (#15)
    
    * testing branch push
    
    * created create profile screen
    
    * enter email screen
    
    * verify email screen
    
    * Add people screen
    
    * Staff and organization auth tree
    
    * git ignored files
    
    ---------
    
    Co-authored-by: Daniel <danieliroka67@gmail.com>
    
    * Strings done
    
    * Finally working
    
    * authentication navigation working
    
    ---------
    
    Co-authored-by: Daniel <danieliroka67@gmail.com>
    Co-authored-by: Idongesit Samuel <robsonidongesitsamuel@gmail.com>

---
+++ app/src/main/java/com/example/titansfreelunch/navigation/FreeLunchNavGraph.kt
@@ -0,0 +1,77 @@
+package com.example.titansfreelunch.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.titansfreelunch.ui.screen.authentication.SignupScreen
+import com.example.titansfreelunch.ui.screen.authentication.organization.SelectedOrganizationSignup
+import com.example.titansfreelunch.ui.screen.authentication.organization.SetupOrganizationScreen
+import com.example.titansfreelunch.ui.screen.authentication.organization.SignupOrganizationDetailsScreen
+import com.example.titansfreelunch.ui.screen.authentication.staff.AddBankDetailsScreen
+import com.example.titansfreelunch.ui.screen.authentication.staff.SelectedStaffSignup
+import com.example.titansfreelunch.ui.screen.authentication.staff.SignupStaffDetailsScreen
+
+
+enum class SignupScreens() {
+    Start,
+    SignupAsStaff,
+    SignupAsOrganization
+}
+
+//host the free lunch navigation graph
+@Composable
+fun FreeLunchNavHost(
+    navHostController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = "Start",
+        modifier = modifier
+    ) {
+        composable(route = "Start"){
+            SignupScreen(
+                onSignupAsOrganizationClicked = { navController.navigate("Organization") },
+                onSignupAsStaffClicked = { navController.navigate("Staff") }
+            )
+        }
+        composable(route = "Staff") {
+            SelectedStaffSignup(
+                onNextStaffButtonClicked = { navController.navigate("StaffSignup") }
+            )
+        }
+        composable(route = "Organization") {
+            SelectedOrganizationSignup(
+                onNextOrganizationButtonClicked = {  navController.navigate("OrganizationSignup") }
+            )
+        }
+        composable(route = "StaffSignup") {
+            SignupStaffDetailsScreen(
+                onStaffDetailsSubmit = {},
+                submitStaffSignupDetails = { navController.navigate("AddStaffBankDetails") }
+            )
+        }
+        composable(route = "OrganizationSignup") {
+            SignupOrganizationDetailsScreen(
+                onOrganizationDetailsSubmit = {},
+                onOrganizationSignupButtonClicked = { navController.navigate("OrganizationDetailsScreen") }
+            )
+        }
+        composable(route = "OrganizationDetailsScreen") {
+            SetupOrganizationScreen(
+                onOrganizationSetupDetailsSubmit = {}
+            )
+        }
+        composable(route = "AddStaffBankDetails") {
+            AddBankDetailsScreen(
+                onAddStaffBankDetailsDone = {}
+            )
+        }
+    }
+
+}
